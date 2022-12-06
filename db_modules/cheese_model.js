621a7dac0b64425133624f7cb38d1046e7e8d575
DevsDame <damianadanulloa@gmail.com>
2022-12-06 15:22:32 -0800

    Created Cheese Model

---
+++ db_modules/cheese_model.js
@@ -0,0 +1,13 @@
+const db = require("../db.js");
+const Sequelize = require('sequelize')
+
+const Cheese = db.define("Cheese", {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    description: Sequelize.STRING,
+})
+
+module.exports = {Cheese};
