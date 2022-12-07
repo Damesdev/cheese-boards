df3c2ff79b65b3933046d1d42764f911406eaf6d
DevsDame <damianadanulloa@gmail.com>
2022-12-06 19:17:37 -0800

    Broken Associations

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const {db} = require("./db");
 const {User} = require("./db_modules/user_model.js");
 const {Cheese} = require("./db_modules/cheese_model");
 const {Board} = require("./db_modules/board_model"); 
-const { Module } = require("module");
 
 // Creating User/Board Association
 User.hasMany(Board);
 Board.belongsTo(User);
 
 // Creating Board/Cheese Assocition
-Board.belongsToMany(Cheese, through: Cheese);
-Cheese.belongsToMany(Board);
+Board.belongsToMany(Cheese, {through: board_cheese});
+Cheese.belongsToMany(Board, {through: board_cheese});
 
 
 module.exports = {Board, Cheese, User};
--- index.test.js
+++ index.test.js
@@ -1,11 +1,8 @@
 // DB Model Imports
-const {User} = require("./db_modules/user_model.js");
-const {Board} = require("./db_modules/board_model")
-const { Cheese } = require("./db_modules/cheese_model.js");
+const {User, Board, Cheese} = require("./index");
+
 
 const {db} = require("./db");
-
-
 
 // User Model Tests
 describe("Testing User Model", () => {
@@ -14,50 +11,94 @@ describe("Testing User Model", () => {
         // the 'sync' method will create tables based on the model class
         // by setting 'force:true' the tables are recreated each time the 
         // test suite is run
-        await db.sync({ force: true });
+        await db.sync({force:true});
+        // bulk creating users
+        User.bulkCreate([
+            {
+                name: "Damian",
+                email: "Damian@gmail.com"
+            },
+            {
+                name: "Abram",
+                email: "Abram@gmail.com"
+            },
+            {
+                name: "Nathaniel",
+                email: "Nathaniel@gmail.com"
+            },
+        ]);
+    
     });
 
     test("Testing User Creation", async () => {
         await User.create({
-            name: "Damian",
-            email: "email@domain.com"
+            name: "Test",
+            email: "Test@domain.com"
         })
 
-        expect(await User.count()).toBe(1)
+        expect(await User.count({where:{name:"Test"}})).toBe(1)
+
     });
 
+    
 });
 
 // Board Model Tests
 describe("Testing Board Model", () =>{
-    // beforeAll(async ()=> {
-    //     await db.sync({force:true})
-    // });
+
+    beforeAll(async () =>{
+        // bulk creating Boards
+        Board.bulkCreate([
+            {type: "Large", description: "Biggest", rating: 3 },
+            {type: "Medium", description: "Middle", rating: 2},
+            {type: "Small", description: "Tiniest", rating: 1},
+        ]);
+    });
 
     test("Testing Board Creation", async () => {
         await Board.create({
-            type: "Greatest",
+            type: "Test",
             description: "Has the good cheeses and good flavors",
             rating: 5,
-        })
+        });
 
-        expect(await Board.count()).toBe(1);
-    })
-})
+        expect(await Board.count({where:{type:"Test"}})).toBe(1);
+    });
+});
 
-// Cheest Model Test
+// Cheese Model Test
 describe("Cheese Model Test", () => {
-    // beforeAll(async () => {
-    //     await db.sync({force:true})
-    // })
+    beforeAll(async () =>{
+        // bulk creating cheeses
+        Cheese.bulkCreate([
+            {title: "Provolone", description: "Round"},
+            {title: "Swiss", description: "Holes"},
+            {title: "American", description: "Yellow"},
+        ])
+    });
 
     test("Testing Cheese creation", async ()=>{
-        expect(await Cheese.count()).toBe(0)
+        // expect(await Cheese.count()).toBe(0)
         await Cheese.create({
-            title: "Pepper Jack",
-            description: "Has peppers and is slightly spicy"
+            title: "Test",
+            description: "for Testing"
         })
-        expect(await Cheese.count()).toBe(1)
+        expect(await Cheese.count({where:{title: "Test"}})).toBe(1)
     })
 
 })
+
+// Creating Association Testing
+describe("Association Testing", () => {
+
+    test("User/Board Association test", async () =>{
+        // finding test board
+        const testBoard = await Board.findOne({where:{type:"Test"}})
+        // finding test User
+        const testUser = await User.findOne({Where: {name:"Test"}})
+
+        await testUser.addBoard()
+    });
+
+
+});
